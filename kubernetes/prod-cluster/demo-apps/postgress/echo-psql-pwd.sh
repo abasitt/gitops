a9c7dc203fca6fb955aedeb8d9c97f0d35788dca
abasitt <abdul.basit@rakuten.com>
2023-08-27 07:21:50 +0000

    updating script

--- kubernetes/prod-cluster/demo-apps/postgress/echo-psql-pwd.sh
+++ kubernetes/prod-cluster/demo-apps/postgress/echo-psql-pwd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-POSTGRES_PASSWORD=""
 
-export "$POSTGRES_PASSWORD"="$(kubectl get secret --namespace demo-apps postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)"
-echo "$POSTGRES_PASSWORD"
+export POSTGRES_PASSWORD=$(kubectl get secret --namespace demo-apps postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
+echo $POSTGRES_PASSWORD
