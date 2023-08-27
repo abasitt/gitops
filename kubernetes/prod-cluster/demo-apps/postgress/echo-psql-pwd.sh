ba108b2e302aabf9c1420646ac140839c76390e6
abasitt <abdul.basit@rakuten.com>
2023-08-27 09:12:27 +0000

    updating demo apps

--- kubernetes/prod-cluster/demo-apps/postgress/echo-psql-pwd.sh
+++ kubernetes/prod-cluster/demo-apps/postgress/echo-psql-pwd.sh
@@ -3,3 +3,6 @@
 
 export POSTGRES_PASSWORD=$(kubectl get secret --namespace demo-apps postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
 echo $POSTGRES_PASSWORD
+
+# read the demo secret
+#k get secrets -n demo-apps vso-db-demo -oyaml -o jsonpath="{.data.password}" | base64 -d
